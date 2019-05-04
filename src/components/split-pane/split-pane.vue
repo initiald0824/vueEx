9455597c0b3208f3bb7d07fed607459123190557
initiald0824 <initiald0824@163.com>
2019-05-04 23:13:24 +0800

    *

--- src/components/split-pane/split-pane.vue
+++ src/components/split-pane/split-pane.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="split-pane-wrapper">
-    <div class="pane pane-left"></div>
+    <div class="pane pane-left" :style="{ width: leftOffsetPercent }"></div>
     <div></div>
-    <div class="pane pane-right"></div>
+    <div class="pane pane-right" :style="{ left: leftOffsetPercent }"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SplitPane'
+  name: 'SplitPane',
+  data () {
+    return {
+      leftOffset: 0.3
+    }
+  },
+  computed: {
+    leftOffsetPercent () {
+      return `${this.leftOffset * 100}%`
+    }
+  }
 }
 </script>
 
@@ -16,8 +26,21 @@ export default {
 .split-pane-wrapper {
   height: 100%;
   width: 100%;
+  position: relative;
   .pane {
+    position: absolute;
+    top: 0;
     height: 100%;
+    &-left {
+      width: 30%;
+      background: red
+    }
+    &-right {
+      right: 0;
+      bottom: 0;
+      left: 30%;
+      background: green
+    }
   }
 }
 </style>
